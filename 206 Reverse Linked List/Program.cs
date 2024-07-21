54455199225a0d67db32e80acf59862ea2fd6986
Zalastiel <zalastiel@kali>
2024-07-21 01:59:00 -0300

    answer for 206

---
+++ 206 Reverse Linked List/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//  * Definition for singly-linked list.
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+ 
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode next;
+        ListNode node = head;
+
+        while (node != null)
+        {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+
+        return prev;
+    }
+}
