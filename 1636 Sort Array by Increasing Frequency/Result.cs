0e08c5ab6415bd73b64ce828ea8dcadb5cce0474
Zalastiel <zalastiel@kali>
2024-07-23 11:54:31 -0300

    describes basic algorithm

---
+++ 1636 Sort Array by Increasing Frequency/Result.cs
@@ -0,0 +1,16 @@
+namespace Result;
+
+public class Solution {
+    public int[] FrequencySort(int[] nums) {
+        /*criar dicionario
+        verificar se a key(numero) ja foi inserida e percorrer a lista
+        1)caso nao. Inserir nova key(numero) e o valor sera 1
+        2)caso sim, acrescentar aumentar o valor em 1
+        
+        ordernar as chaves pela quantidade de valores
+        criar novo array
+        
+        */
+        return null;
+    }
+}
